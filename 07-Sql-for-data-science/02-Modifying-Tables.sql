cc90c72ff8776cc8cf6310088fa9deb55f7b75a9
Ali Ahmed <alyahmedrbk@gmail.com>
2025-05-26 18:03:23 +0500

    Deleting and drop to delete content of table and table itself

--- 07-Sql-for-data-science/02-Modifying-Tables.sql
+++ 07-Sql-for-data-science/02-Modifying-Tables.sql
@@ -12,7 +12,7 @@ create table employee(
 
 rename table employee to employee_data;
 
-insert into	employee_data values (01, 'Ali Ahmed', 24, 'Male', 50000);
+insert into	employee_data values (01, 'Ali Ahmed', 'Male',24,  50000);
 
 select * from employee_data;
 
@@ -31,3 +31,7 @@ ALTER table employee_data ADD column age int default 24;
 Alter table employee_data modify column name varchar(100) default ('');
 
 alter table employee_data modify column Salary int AFTER age;
+
+delete from employee_data where id=1;
+
+drop table employee_data;
